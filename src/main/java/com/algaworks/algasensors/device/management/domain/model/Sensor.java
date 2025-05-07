2c283ca7adfcb2eef6d3b3d610c5afa317021dca
RE045908 <re045908@qintess.com>
2025-05-07 11:07:03 -0300

    Organize imports

--- src/main/java/com/algaworks/algasensors/device/management/domain/model/Sensor.java
+++ src/main/java/com/algaworks/algasensors/device/management/domain/model/Sensor.java
@@ -1,11 +1,9 @@
 package com.algaworks.algasensors.device.management.domain.model;
 
-import io.hypersistence.tsid.TSID;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
